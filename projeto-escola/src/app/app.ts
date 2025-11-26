5a1adcfd7def548f87b216c53d2f2447a46fd2f3
Guilherme Araujo <gui.araujo1902@gmail.com>
2025-11-26 20:59:12 -0300

    feat(components): Ajuste em Student

--- projeto-escola/src/app/app.ts
+++ projeto-escola/src/app/app.ts
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'projeto-escola';
+
+  titulo = 'Sistema Alunos Proway';
+
+  escola = {
+    nome: 'Proway',
+    cidade: 'Blumenau'
+  }
+
+  listaAlunos = [
+    {
+      nome: 'Harry',
+      matricula: 111,
+      curso: 'Frontend',
+      email: 'harry@proway.com',
+      materias: ['HTML', 'CSS'],
+      imagem: 'harry.png'
+    }, 
+    {
+      nome: 'Hermione',
+      matricula: 222,
+      curso: 'Backend',
+      email: 'hermione@proway.com',
+      materias: ['Java', 'PostgreSql'],
+      imagem: 'hermione.png'
+    },
+    {
+      nome: 'Rony',
+      matricula: 333,
+      curso: 'Dados',
+      email: 'rony@proway.com',
+      materias: ['Python', 'BI'],
+      imagem: 'rony.png'
+    }
+  ]
 }
--- projeto-escola/src/app/components/student/student.ts
+++ projeto-escola/src/app/components/student/student.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-student',
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './student.css',
 })
 export class Student {
+
+  @Input() aluno!: any
+  @Input() escola!: any
 
 }
